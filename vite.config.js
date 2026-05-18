9a7ff5851921cbce6e5b64e3f17984f543f5a6bc
sagar2018 <sagar2018@gmail.com>
2026-05-18 11:08:48 +0530

    new production ready

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig({
-    base: '/jdaks/public/build/',
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        base: env.VITE_BASE_PATH || '/build/',
+        plugins: [
+            laravel({
+                input: ['resources/css/app.css', 'resources/js/app.js'],
+                refresh: true,
+            }),
+        ],
+    };
 });
